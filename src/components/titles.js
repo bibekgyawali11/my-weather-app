19616eaf980c44a5650703b9259624929093959f
Bibek Gyawali <51143581+bibekgyawali11@users.noreply.github.com>
2020-10-08 16:00:41 -0500

    Update titles.js

--- src/components/titles.js
+++ src/components/titles.js
@@ -4,7 +4,7 @@ class Titles extends React.Component{
     render(){
         return(
         <div>
-            <h1 className="title-container__title">Bibek Instant Weather Finder</h1>
+            <h1 className="title-container__title">Bibek Instant Weather Finder Test</h1>
             <p className="title-container__subtitle"> Find out temparature, conditions and more..... </p>
         </div>
         );
